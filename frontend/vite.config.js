a6b2677cfe6e12949700a0e2a8e0a494414aec7b
Bhavith S <bhaviths15@gmail.com>
2026-03-29 17:54:36 +0530

    feat: configure PWA with Vite-Plugin-PWA and custom branding

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: ["logo.svg"],
+      manifest: {
+        id: '/',
+        name: "ATTSYS 2.0: Automated Attendance",
+        short_name: "ATTSYS 2.0",
+        description: "Next-gen automated attendance tracking ecosystem.",
+        theme_color: "#000000",
+        background_color: "#000000",
+        scope: '/',
+        start_url: '/',
+        display: "standalone",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "logo.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+            purpose: "any",
+          },
+          {
+            src: "logo.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
